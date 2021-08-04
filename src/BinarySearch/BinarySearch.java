92d3d53d86dda9ab33206997b0148a15e616eb73
Emirality <vlad.burak1997@list.ru>
2021-08-04 20:06:13 +0300

    ex

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -1,3 +1,5 @@
+package BinarySearch;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
---
+++ src/BinarySearch/Ex.txt
@@ -0,0 +1,1 @@
+![alt text](https://mikebuss.com/assets/images/posts/binary-search/BinarySearch.png)
